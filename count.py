7d3f6371077746d38940d8d02bffb7cd28035546
riyakamat-0605 <riyakamat2006@gmail.com>
2025-11-27 15:36:14 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,20 +1,24 @@
 import sys
 
 if len(sys.argv) > 1:
-    nums = list(map(int, sys.argv[1:]))
-    print("User provided list:")
+    script_name = sys.argv[0]
+    numbers = [int(x) for x in sys.argv[1:]]
+    print("User provided input numbers:")
 else:
-    nums = [1, 2, 3, 4, 5, 6]
-    print("No input given — using default list [1, 2, 3, 4, 5, 6]:")
+    script_name = sys.argv[0]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print("No input given – using default list:")
 
 even_count = 0
 odd_count = 0
 
-for n in nums:
+for n in numbers:
     if n % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
 
-print("Even numbers:", even_count)
-print("Odd numbers:", odd_count)
+print("Script Name:", script_name)
+print("Numbers:", numbers)
+print("Count of even numbers:", even_count)
+print("Count of odd numbers:", odd_count)
